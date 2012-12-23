221e7fa88a20e0741e5c0855a40935850daf2d76
Ray Racine <ray.racine@gmail.com>
2012-12-22 23:21:07 -0500

    One generic AWS error structure which is thrown.

--- dynamodb/error.rkt
+++ dynamodb/error.rkt
@@ -21,75 +21,41 @@
 ;; "{\"message\": \"Supplied AttributebValue is empty, must contain exactly one of the supported datatypes\", \"__type\": \"com.amazon.coral.validate#ValidationException\"}"
 
 (provide
- throw is-exception-response? ddb-failure
- DDBFailure DDBFailure? 
- illformed-response
- ConditionalCheckFailed ConditionalCheckFailed?
- ValidationException ValidationException?
- ResourceNotFound ResourceNotFound?
- InUseException InUseException?
- InvokeConditionsNotMet InvokeConditionsNotMet?)
-
+ AWSFailure
+ ;throw 
+ is-exception-response? aws-failure)
+ 
 (require 
  (only-in "../../format/json/tjson.rkt"
           Json JsObject json->string string->json jsobject))
 
-(struct: DDBFailure exn:fail () #:transparent)
-
-(struct: InvokeConditionsNotMet DDBFailure () #:transparent)
-
-(struct: IllFormedResponse DDBFailure ([json : String]) #:transparent)
-
-(struct: ValidationException DDBFailure () #:transparent)
-
-(struct: InUseException DDBFailure () #:transparent)
-
-(struct: ResourceNotFound DDBFailure () #:transparent)
-
-(struct: ConditionalCheckFailed DDBFailure () #:transparent)
+(struct: AWSFailure exn:fail ([type : String]) #:transparent)
 
 (define unknown-msg "Unknown error message type: ")
 (define ill-formed-msg "Unparsable error response")
 
-(define-syntax throw
-  (syntax-rules ()
-    ((throw excn)
-     (raise (ddb-failure excn #t)))))
+;(define-syntax throw
+;  (syntax-rules ()
+;    ((throw excn)
+;     (raise (ddb-failure excn #t)))))
 
 (: is-exception-response? (JsObject -> Boolean))
 (define (is-exception-response? jsobj)
   (and (hash-has-key? jsobj 'message)
        (hash-has-key? jsobj '__type)))
 
+(: malformed-response (String -> Nothing))
+(define (malformed-response resp)
+  (raise (AWSFailure resp (current-continuation-marks) "MalformedJSON")))
 
-(: illformed-response (String -> Nothing))
-(define (illformed-response resp)
-  (raise (IllFormedResponse unknown-msg (current-continuation-marks) resp)))
-
-(: ddb-failure (JsObject -> Nothing))
-(define (ddb-failure jsobj)
-  
-  (: raise-illformed (String -> Nothing))
-  (define (raise-illformed msg)
-    (raise (IllFormedResponse ill-formed-msg 
-                              (current-continuation-marks) 
-                              msg)))
-  
+(: aws-failure (JsObject -> Nothing))
+(define (aws-failure jsobj)      
   (if (is-exception-response? jsobj)
       (let ((type (hash-ref jsobj '__type))
             (msg  (hash-ref jsobj 'message)))
         (if (and (string? type)
                  (string? msg))
-            (cond
-              ((string=? type "com.amazon.coral.validate#ValidationException")
-               (raise (ValidationException msg (current-continuation-marks))))
-              ((string=? type "com.amazonaws.dynamodb.v20111205#ResourceInUseException")
-               (raise (InUseException msg (current-continuation-marks))))
-              ((string=? type "com.amazonaws.dynamodb.v20111205#ResourceNotFoundException")
-               (raise (ResourceNotFound msg (current-continuation-marks))))
-              ((string=? type "com.amazonaws.dynamodb.v20111205#ConditionalCheckFailedException")
-               (raise (ConditionalCheckFailed msg (current-continuation-marks))))
-              (else (raise-illformed (json->string jsobj))))
-            (raise-illformed (json->string jsobj))))
-      (raise-illformed (json->string jsobj))))
+            (raise (AWSFailure msg (current-continuation-marks) type))
+            (malformed-response (json->string jsobj))))
+      (malformed-response (json->string jsobj))))
 
