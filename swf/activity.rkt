7a9860d6106f3fc3e33d298cbe2ed9db13de967a
Ray Racine <ray.racine@gmail.com>
2013-01-22 09:48:44 -0500

    Add missing GPLv3 license.

--- swf/activity.rkt
+++ swf/activity.rkt
@@ -1,151 +1,41 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Ray Racine's Amazon API Library
+;; Copyright (C) 2007-2013  Raymond Paul Racine
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+#| Data structures and response building routines for Activity task responses. |#
+
 #lang typed/racket/base
 
-(provide
- (struct-out DecisionTask)
- (struct-out ActivityTask)
- poll-for-decision-task
- poll-for-activity-task)
+(provide:
+ [respond-activity-task-completed (String String -> Void)])
 
-(require 
- racket/pretty
- (only-in racket/function
-          thunk)
- (only-in prelude/std/opt
-          opt-getorelse)
+(require
  (only-in format/json/tjson
-          JsObject Json json->string jsobject jsobject-opt)
- (only-in "types.rkt"
-          WorkflowExecution 
-          WorkflowType ActivityType 
-          Input TaskToken)
- (only-in "attrs.rkt"
-          parse-history-events
-          parse-workflow-type
-          parse-workflow-execution)
- (only-in "historyevent.rkt"
-          HistoryEvent)
- (only-in "../dynamodb/parse.rkt"          
-          attr-value-jsobject attr-value-jslist
-          attr-value-integer attr-value-integer-opt
-          attr-value-string attr-value-string-opt)
+          JsObject jsobject jsobject-opt)
  (only-in "../dynamodb/invoke.rkt"
           workflow))
 
-(define-type TaskList String)
+(define respond-activity-task-completed-target "SimpleWorkflowService.RespondActivityTaskCompleted")
 
-(struct: ActivityTask ([id : String]
-                       [type : ActivityType]
-                       [input : Input]
-                       [started-event-id : Integer]
-                       [token : TaskToken]
-                       [workflow : WorkflowExecution]) #:transparent)
-
-(struct: DecisionTask ([task-token : String]
-                       [events : (Listof HistoryEvent)]
-                       [next-page-token : (Option String)]
-                       [started-event-id : Integer]                      
-                       [previous-started-event-id : Integer]                       
-                       [workflow-execution : WorkflowExecution]
-                       [workflow-type : WorkflowType]) #:transparent)
-
-(define poll-for-activity-target "SimpleWorkflowService.PollForActivityTask")
-(define poll-for-decision-target "SimpleWorkflowService.PollForDecisionTask")
-
-(: no-available-activity (JsObject -> Boolean))
-(define (no-available-activity jsobj)
-  (let ((started-event-id (attr-value-integer jsobj 'startedEventId)))
-    (zero? started-event-id))) ;; long poll timed out with a response from AWS of no activity received
-
-;; Note a proper response may be a "no task" response, which is why the return value is optional.
-(: parse-activity-task (JsObject -> (Option ActivityTask)))
-(define (parse-activity-task json)
-  ;;(pretty-print json)
-  (let ((started-event-id (attr-value-integer json 'startedEventId)))
-    (if (no-available-activity json) ;; timedout with no activity received
-        #f
-        (ActivityTask "id" (ActivityType "name" "ver") 
-                      "inputkadfafadf"  started-event-id "token" 
-                      (WorkflowExecution "wf-id" "run-id")))))
-
-;; AWS long polls, so a call will "hang" for 60 secs prior to returning.
-(: poll-for-activity-task (String (Option String) String -> TaskList))
-(define (poll-for-activity-task domain identity task-list)
-  (let ((payload (jsobject-opt `((domain . ,domain)
-                                 (identity . ,identity)
-                                 (taskList . ,(jsobject `((name . ,task-list))))))))
-    ;;(pretty-print payload)
-    (json->string (workflow poll-for-activity-target payload))))
+(: respond-activity-task-completed (String String -> Void))
+(define (respond-activity-task-completed task-token result)
+  (workflow respond-activity-task-completed-target (jsobject `((result . ,result)
+							       (taskToken . ,task-token))))
+  (void))
+  
 
 
-					;'#hasheq((previousStartedEventId . 0)
-					;         (events
-					;          .
-					;          (#hasheq((eventId . 1)
-					;                   (workflowExecutionStartedEventAttributes
-					;                    .
-					;                    #hasheq((input . "my input data here")
-					;                            (taskList . #hasheq((name . "mr-master")))
-					;                            (parentInitiatedEventId . 0)
-					;                            (taskStartToCloseTimeout . "1200")
-					;                            (executionStartToCloseTimeout . "7200")
-					;                            (childPolicy . "TERMINATE")
-					;                            (workflowType
-					;                             .
-					;                             #hasheq((name . "mapreduce") (version . "1.0")))))
-					;                   (eventType . "WorkflowExecutionStarted")
-					;                   (eventTimestamp . 1.3569767483749))
-					;           #hasheq((eventId . 2)
-					;                   (decisionTaskScheduledEventAttributes
-					;                    .
-					;                    #hasheq((taskList . #hasheq((name . "mr-master")))
-					;                            (startToCloseTimeout . "1200")))
-					;                   (eventType . "DecisionTaskScheduled")
-					;                   (eventTimestamp . 1.3569767483749))
-					;           #hasheq((eventId . 3)
-					;                   (decisionTaskStartedEventAttributes
-					;                    .
-					;                    #hasheq((scheduledEventId . 2)))
-					;                   (eventType . "DecisionTaskStarted")
-					;                   (eventTimestamp . 1.35697734219))))
-					;         (workflowType . #hasheq((name . "mapreduce") (version . "1.0")))
-					;         (startedEventId . 3)
-					;         (workflowExecution
-					;          .
-					;          #hasheq((runId . "11KxU8+9lE8BB+7F2FMTAfwBOb+LxeEtjG2gQBf9YEl2U=")
-					;                  (workflowId . "9420161c-4f32-112d-9b8b-0090f5ccb571")))
-					;         (taskToken
-					;          .
-					;          "AAAAKgAAAAEAAAAAAAAAAbhOa7pTSREnSf3IsydwCMC/k3Nhx1H2RXEiA8TwMobm7isRaNtiVMNUgjhNqBd4xtb2TloC5jaQIovzOpRnNRWFC6ZgPtGvJVyG4Cvq2rpNlAEfevRNGEwEHhc4dxmqsh+2dNaoL6Pio0rA6Ud3JX7K8JHOnAxxiEDptqbEMmqQD48W002ZCV4dIIdW9Jehaq72aAELQKsGEIoc+6BaetFI3ps2zzU7yjQCyIni2klAotT387fEPw6ArPcmUWh8qowsc/yVZDmH01oP6FqZecpYF9KGlTy+wBS2qq8sCqg9IZaRMdmXJuphRv7gpkcClw=="))
-					;- : (U False DecisionTask)
-					;(DecisionTask '() "" 0 0 "" #<WorkflowExecution> #<WorkflowType>)
-
-;; Decision Task API
-
-(: parse-decision-task-response (JsObject -> (Option DecisionTask)))
-(define (parse-decision-task-response jsobj)
-  (if (no-available-activity jsobj)
-      #f
-      (DecisionTask (attr-value-string jsobj 'taskToken)
-                    (parse-history-events (attr-value-jslist jsobj 'events))
-                    (attr-value-string-opt jsobj 'nextPageToken)
-                    (attr-value-integer jsobj 'startedEventId)
-                    (opt-getorelse (attr-value-integer-opt jsobj 'previousStartedEventId)
-                                   (thunk (attr-value-integer jsobj 'startedEventId)))
-                    (parse-workflow-execution (attr-value-jsobject jsobj 'workflowExecution))
-                    (parse-workflow-type (attr-value-jsobject jsobj 'workflowType)))))
-
-;; AWS long polls, so a call will "hang" for 60 secs prior to returning.
-(: poll-for-decision-task (case-> (String String -> (Option DecisionTask))
-                                  (String String String (Option Integer) String Boolean -> (Option DecisionTask))))
-(define (poll-for-decision-task domain task-list 
-                                [identity ""] 
-                                [max-page-size #f] 
-                                [next-page-token ""] 
-                                [reverse-order #f])
-  (parse-decision-task-response (workflow poll-for-decision-target 
-                                          (jsobject-opt `((domain . ,domain)
-                                                          (identity . ,identity)
-                                                          (maximumPageSize . ,max-page-size)
-                                                          (nextPageToken . ,next-page-token)
-                                                          (reverseOrder . ,reverse-order)
-                                                          (taskList . ,(jsobject `((name . ,task-list)))))))))
--- swf/decision.rkt
+++ swf/decision.rkt
@@ -1,8 +1,28 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Ray Racine's Amazon API Library
+;; Copyright (C) 2007-2013  Raymond Paul Racine
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+#| Data structures and response building routines for Decision task responses. |#
+
 #lang typed/racket/base
 
 (provide:
-[fail-workflow-execution-decision (case-> (-> JsObject)
-					  (String String -> JsObject))]
+ [fail-workflow-execution-decision (case-> (-> JsObject)
+					   (String String -> JsObject))]
  [respond-decision-task-completed (String String (Listof JsObject) -> Void)]
  [signal-external-workflow-execution-decision SignalExternalWorkflowExecutionDecisionFn]
  [continue-as-new-workflow-decision ContinueAsNewWorkflowDecisionFn]
@@ -131,7 +151,7 @@ and records a WorkflowExecutionFailed event in the history. |#
                                                                               (details . ,details)))))))
 
 #| CancelWorkflowExecution closes the workflow execution 
-   and records a WorkflowExecutionCanceled event in the history. |#
+and records a WorkflowExecutionCanceled event in the history. |#
 (: cancel-workflow-execution-decision (case-> (-> JsObject)
                                               (String -> JsObject)))
 (define (cancel-workflow-execution-decision [details ""])
@@ -198,9 +218,11 @@ a RequestCancelExternalWorkflowExecutionInitiated event in the history. |#
   (jsobject-opt `((decisionType . "RequestCancelExternalWorkflowExecution")              
                   (requestCancelExternalWorkflowExecutionDecisionAttributes . ,attrs))))
 
-#| StartChildWorkflowExecution requests that a child workflow execution be started 
+#|--------------------------------------------------------------------------------- 
+StartChildWorkflowExecution requests that a child workflow execution be started 
 and records a StartChildWorkflowExecutionInitiated event in the history. 
-The child workflow execution is a separate workflow execution with its own history. |#
+The child workflow execution is a separate workflow execution with its own history. 
+-----------------------------------------------------------------------------------|#
 (: start-child-workflow-execution-decision StartChildWorkflowExecutionDecisionFn)
 (define (start-child-workflow-execution-decision workflow-id workflow-type
                                                  #:child-policy [policy #f] 
